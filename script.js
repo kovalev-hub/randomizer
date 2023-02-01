11a8ffce32457dd1815200274c409db724a53b43
Igor <110007741+kovalev-hub@users.noreply.github.com>
2023-02-01 03:40:45 -0800

    fix: text correction

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const BUTTONSTART = document.querySelector(`.button-randomizer-start`);
 const RESULT = document.querySelector(`.randomizer-result`);
 
 BUTTONSTART.addEventListener(`click`, function () {
-  let randomizerRandge = prompt(`PRESS RANDOMIZER RANGE:`);
+  let randomizerRandge = prompt(`Enter randomizer range:`);
   randomizerRandge = Number(randomizerRandge);
   RESULT.innerHTML = Math.floor(Math.random() * randomizerRandge);
 })
